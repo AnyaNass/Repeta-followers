37d23d506f7a686777e051b7feaf1f7881e17fee
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-07-29 14:52:02 +0300

    contacts is over

--- src/index.js
+++ src/index.js
@@ -22,3 +22,12 @@
 		body.classList.toggle('no-scroll');
 	}
 })();
+
+// Скрипт для форми телефону
+
+const phoneMask = IMask(
+	document.getElementById('user-phone'), {
+	mask: '+{38}(\\000)000 00 00',
+	lazy: false,  // make placeholder always visible
+	placeholderChar: ' '     // defaults to '_'
+});

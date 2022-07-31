9badfe3ad4563fab1e43fbdc71f08e0ac3fa40b8
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2022-07-31 13:36:00 +0300

    add slick

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./js/mask"
 import "./js/modal"
+import "./js/slider"
+import "./js/read-more"
---
+++ src/js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.reviews-cards-list').slick({
+        dots: true,
+        speed: 700,
+        infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+        // pauseOnFocus: true,
+        // pauseOnHover: true,
+        // pauseOnDotsHover: true
+    });
+});
